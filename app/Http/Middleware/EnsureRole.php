afa6d3b3df45611ddf8f3a31d1ea7f59a90ca07d
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2025-08-23 14:21:12 -0300

    feat: new middleware

---
+++ app/Http/Middleware/EnsureRole.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        /** @var User $user */
+        $user = $request->user();
+
+        if (! $user || ! $user->hasAnyRole($roles)) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()
+            ->asAdmin()
+            ->create([
+                'name'  => 'Admin',
+                'email' => 'test@example.com',
+            ]);
+
+        User::factory(5)->asEmployee()->create();
+        User::factory(15)->asCustomer()->create();
+    }
+}
