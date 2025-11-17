800e4298ce485268e402b08b1cfea794798f2b00
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2025-11-17 12:29:21 -0300

    Build front-end para produção

---
+++ public/build/assets/status-badge-BcZv8WiZ.js
@@ -0,0 +1,6 @@
+import{c as t}from"./createLucideIcon-CdDXl1n5.js";import{j as e}from"./app-BTymDE-b.js";import{B as a}from"./badge-FDCXrNx6.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z",key:"yt0hxn"}],["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}]],s=t("Bolt",c);function l({status:r}){switch(r){case"active":return e.jsx(a,{className:"bg-emerald-600 hover:bg-emerald-600",children:"Ativo"});case"pre_active":return e.jsx(a,{className:"bg-amber-500 hover:bg-amber-500",children:"Pré-ativo"});case"inactive":return e.jsx(a,{variant:"secondary",children:"Inativo"});default:return e.jsx(a,{variant:"outline",children:r})}}export{s as B,l as S};
